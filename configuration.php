f827c50f9ef164188470d271291605c5a20c2818
valeriekebz <valeriekebz@gmail.com>
2023-01-23 14:29:32 +0300

    configurations

--- configuration.php
+++ configuration.php
@@ -75,7 +75,7 @@ DEVCO Chair Advisory Group';
 	public $unicodeslugs = '0';
 	public $feed_limit = '10';
 	public $feed_email = 'none';
-	public $log_path = '/kebsite/logs';
+	public $log_path = '/logs';
 	public $tmp_path = '/tmp';
 	public $lifetime = '15';
 	public $session_handler = 'database';
